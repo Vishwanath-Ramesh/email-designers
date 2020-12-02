4bc3233b2d2326a98caf9c93e74d4df4a93b6302
Vishwanath R <vishwanath@LogicValley.in>
2020-12-02 18:46:01 +0530

    Upon completion of Learning and development

---
+++ README.md
@@ -0,0 +1,1 @@
+# A React application created while learning CSS Layout and Animations in https://designcode.io/
---
+++ src/views/Routes/Routes.js
@@ -0,0 +1,26 @@
+import React, { lazy, Suspense } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+const PageNotFound = lazy(() => import('../pages/PageNotFound/PageNotFound'))
+const Home = lazy(() => import('../pages/Home/Home'))
+
+const Routes = () => {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/404" component={PageNotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </Suspense>
+    </Router>
+  )
+}
+
+export default Routes
